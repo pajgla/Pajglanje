57233628d3df8df5fd21e1a19ea6b76ee8eebb13
Aleksandar Zdravković <pajgla@users.noreply.github.com>
2025-10-25 01:14:49 +0200

    Adding browser cache configuration
    - On mobile phones, some modules were cached and not updated even after a file was updated on our side, so I added hash to all TS modules, which will force browsers to download the module if the name of the cached module isn't equal to server's version

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
         brzalica: 'brzalica.html',
         tragalica: 'tragalica.html',
         maintenance: 'maintenance.html'
+      },
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
   }
