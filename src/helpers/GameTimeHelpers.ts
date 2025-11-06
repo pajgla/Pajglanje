3bcdfdc0ea4cb27a77983a25a95a59464acfbe85
Aleksandar Zdravković <pajgla@users.noreply.github.com>
2025-11-06 23:10:56 +0100

    Fixing daylight saving problem

--- src/helpers/GameTimeHelpers.ts
+++ src/helpers/GameTimeHelpers.ts
@@ -1,6 +1,7 @@
 export class GameTimeHelpers {
     public static GetGameTime(gameStartDate: Date, hoursBetweenGames: number): number {
         const currentDate = new Date();
+        currentDate.setHours(currentDate.getHours() - 1); // -1 for daylight saving time, remove on summer
         const dateDifference = currentDate.getTime() - gameStartDate.getTime();
         let hourDiff = Math.floor(dateDifference / (1000 * 60 * 60));
         return Math.floor(hourDiff / hoursBetweenGames);
